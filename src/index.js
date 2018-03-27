2006fdbebebfd9d1c24cbe0e7bc18a14e3c33852
Steponas Ivasovas <steponas@hotmail.com>
2018-03-27 23:38:23 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-/*
-namas {
-  id:
-  info:
-  miestas:
-  rajonas:
-  gatve:
-}
-
-miestas {
-  id:
-  info:
-}
-
-rajonas {
-  id:
-  info:
-  miestas:
-}
-
-gatve {
-  id:
-  info:
-  rajonas:
-  miestas:
-}
-*/
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DATA from './data.js';
@@ -38,26 +9,44 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: this.props.data,
       selected: Object.keys(this.props.data)[0],
       page: 0,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handlePageClick = this.handlePageClick.bind(this);
+    this.handleInfoChange = this.handleInfoChange.bind(this);
   }
   handleClick(category) {
-    this.setState({selected: category});
+    this.setState({
+      selected: category,
+      page: 0,
+    });
   }
   handlePageClick(page) {
     this.setState({page: page});
   }
+  handleInfoChange(id, value) {
+    const newData = {...this.state.data};
+    const categoryData = newData[this.state.selected];
+    const index = categoryData.findIndex(element => element.id == id);
+    const item = categoryData[index];
+    const newItem = {...item, info: value};
+    categoryData[index] = newItem;
+
+    this.setState({
+      data: newData
+    });
+  }
   render() {
     const categories = Object.keys(this.props.data);
-    const displayData = this.props.data[this.state.selected];
+    const displayData = this.state.data[this.state.selected];
 
     return (
       <React.Fragment>
         <Sidebar onSidebarItemClick={this.handleClick} categories={categories}/>
         <MainBody
+          onInfoChange={this.handleInfoChange}
           onPageClick={this.handlePageClick}
           data={displayData}
           page={this.state.page}
@@ -98,9 +87,13 @@ class MainBody extends React.Component {
   constructor(props) {
     super(props);
     this.handlePageClick = this.handlePageClick.bind(this);
+    this.handleInfoChange = this.handleInfoChange.bind(this);
   }
   handlePageClick(page) {
     this.props.onPageClick(page);
+  }
+  handleInfoChange(id, value) {
+    this.props.onInfoChange(id, value);
   }
   render() {
     const filtered = this.props.data.slice(this.props.page * perPage,
@@ -109,7 +102,7 @@ class MainBody extends React.Component {
     return (
       <div className="main-body">
         <button className="btn-add">Pridet nauja</button>
-        <DataList data={filtered}/>
+        <DataList data={filtered} onInfoChange={this.handleInfoChange}/>
         <NavPages
           onPageClick={this.handlePageClick}
           pages={Math.ceil(this.props.data.length / perPage)}
@@ -121,15 +114,22 @@ class MainBody extends React.Component {
 }
 
 class DataList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleInfoChange = this.handleInfoChange.bind(this);
+  }
   componentDidMount() {
-    this.height = this.el.offsetHeight;
+    // this.height = this.el.offsetHeight;
   }
   componentDidUpdate() {
+  }
+  handleInfoChange(id, value) {
+    this.props.onInfoChange(id, value);
   }
   render() {
     const dataItems = this.props.data.map(dataItem => {
       return (
-        <DataRow dataItem={dataItem} key={dataItem.id}/>
+        <DataRow dataItem={dataItem} key={dataItem.id} onInfoChange={this.handleInfoChange}/>
       );
     });
 
@@ -142,13 +142,61 @@ class DataList extends React.Component {
 }
 
 class DataRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editable: false,
+      value: this.props.dataItem.info,
+    };
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleSaveClick = this.handleSaveClick.bind(this);
+    this.handleInfoChange = this.handleInfoChange.bind(this);
+    this.handleCancelClick = this.handleCancelClick.bind(this);
+  }
+  handleCancelClick() {
+    this.setState({
+      value: this.props.dataItem.info,
+      editable: false,
+    })
+  }
+  handleEditClick() {
+    this.setState({editable: true});
+  }
+  handleSaveClick(id) {
+    this.setState({editable: false});
+    this.props.onInfoChange(id, this.state.value);
+  }
+  handleInfoChange(event) {
+    this.setState({value: event.target.value});
+  }
+  componentDidUpdate() {
+    if (this.input) {
+      // this.input.focus();
+      // const info = this.state.value;
+      // this.input.selectionStart = info.length;
+      // this.input.selectionEnd = info.length;
+    }
+  }
   render() {
     const dataItem = this.props.dataItem;
 
     return (
       <li>
-        <span>{dataItem.info}</span>
-        <button>Redaguoti</button>
+        {this.state.editable ?
+          <textarea
+            value={this.state.value}
+            onChange={this.handleInfoChange}
+            ref={(input) => (this.input = input)}
+          />:
+          dataItem.info
+        }
+        {this.state.editable ?
+          (<div>
+            <button onClick={this.handleSaveClick.bind(this, dataItem.id)}>Issaugoti</button>
+            <button onClick={this.handleCancelClick}>Atsaukti</button>
+          </div>) :
+          <button onClick={this.handleEditClick}>Redaguoti</button>
+        }
       </li>
     );
   }
@@ -160,6 +208,7 @@ class NavPages extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(page) {
+    if (page < 0 || page >= this.props.pages) return;
     this.props.onPageClick(page);
   }
   render() {
@@ -167,25 +216,31 @@ class NavPages extends React.Component {
     const count = this.props.pages;
 
     if (count > 1) {
-      if (count > 2 && this.props.current > 0) {
+      if (count >= 2) {
+        const className = (this.props.current === 0) ? 'disabled' : '';
         pages.push(
-          <li onClick={this.handleClick.bind(this, this.props.current - 1)}>
+          <li className={className}
+              onClick={this.handleClick.bind(this, this.props.current - 1)}>
             &lt;
           </li>
         );
       }
 
       for (let i = 0; i < this.props.pages; i++) {
+        const className = (i === this.props.current) ? 'active' : '';
         pages.push(
-          <li onClick={this.handleClick.bind(this, i)}>
+          <li className={className}
+              onClick={this.handleClick.bind(this, i)}>
             {i + 1}
           </li>
         );
       }
 
-      if (count > 2 && this.props.current < count - 1) {
+      if (count >= 2) {
+        const className = (this.props.current === count - 1) ? 'disabled' : '';
         pages.push(
-          <li onClick={this.handleClick.bind(this, this.props.current + 1)}>
+          <li className={className}
+              onClick={this.handleClick.bind(this, this.props.current + 1)}>
             &gt;
           </li>
         );
